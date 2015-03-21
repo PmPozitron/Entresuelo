55d63d8600f7ad55cc880cd06bfab029a68e156b
Andrey Galaktionov <andrey.galaktionov@smartms.org>
2015-03-21 13:01:07 +0400

    target deleted

--- target/Entresuelo/WEB-INF/sql/drop_create_db.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE IF EXISTS entresuelo;
-
-CREATE DATABASE entresuelo CHARACTER SET utf8 COLLATE utf8_general_ci;
-
-CREATE USER spring IDENTIFIED BY 'security';
-GRANT ALL PRIVILEGES ON entresuelo TO spring;
